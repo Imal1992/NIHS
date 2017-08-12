6e37ca4cb5aae19f43c995851acbddd9db8f6591
Senith <rsenith@gmail.com>
2017-08-12 23:08:00 +0530

    admin can upload photos to web site

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -134,19 +134,19 @@
         <div id="maindiv">
 
             <div id="formdiv">
-                <h2>Multiple Advertisment Upload Form</h2>
+                <h2>Photo Upload Form</h2>
                 <form enctype="multipart/form-data" action="" method="post">
                     First Field is Compulsory. Only JPEG,PNG,JPG Type Image Uploaded. Image Size Should Be Less Than 2MB.
                     <hr/>
 
                     <div id="filediv">
-                        <input name="file[]" type="file" id="file"/>
+                        <input name="files[]" type="file" id="file"/>
                     </div>
 
                     <br/>
            
-                    <input type="button" id="add_more" class="upload" value="Add More Files"/>
-                    <input type="submit" value="Upload File" name="submit" id="upload" class="upload"/>
+                    <input type="button" id="add_more" class="upload" value="Add More Photos"/>
+                    <input type="submit" value="Upload Photos" name="submit" id="upload" class="upload"/>
                 </form>
                 <br/>
                 <br/>
