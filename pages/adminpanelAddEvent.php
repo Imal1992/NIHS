c366ac64c2885c6fe3c66eb05e03980ca4518888
imal1992 <ithiunuwan@gmail.com>
2017-08-28 19:00:20 +0530

    Created Image Gallery

--- pages/adminpanelAddEvent.php
+++ pages/adminpanelAddEvent.php
@@ -1,3 +1,13 @@
+<?php
+
+    session_start();  /* Start of the session */
+
+    if(!isset($_SESSION['adminLoggedIn'])){
+        header("location: signin.php");
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -49,10 +59,6 @@
             </div>
         </div>
 
-        <!-- <form id = "Sign_Out" action ="../backend files/logout.php" method = "get">
-            <input type = "submit" value = "Sign Out" id="signoutButton"/> -->   <!-- Setting logout button-->
-
-        <!-- </form> -->
     </div>
     <!-- End: Navigation wrapper -->
 </header>
@@ -60,65 +66,6 @@
 <!-- Start: Main content -->
 <div class="content">
     <div class="container">
-        <!-- Start: Service description -->
-        <!-- <article class="article">
-            <div class="row bottom-space">
-                <div class="span12">
-                    <div class="page-header">
-                        <h1>Service <small>Caption for the service</small></h1>
-                    </div>
-                </div>
-                <div class="span12 center-align">
-                    <img src="http://placehold.it/800x300" class="thumbnail product-snap">
-                </div>
-            </div>
-            <div class="row bottom-space">
-                <div class="span10 offset1">
-                    <p>
-                        Detailed description about the service that how it makes the customer's life easy.
-                        Detailed description about the service that how it makes the customer's life easy.
-                        Detailed description about the service that how it makes the customer's life easy.
-                        Detailed description about the service that how it makes the customer's life easy.
-                    </p>
-                    <p>
-                        Detailed description about the service that how it makes the customer's life easy.
-                        Detailed description about the service that how it makes the customer's life easy.
-                        Detailed description about the service that how it makes the customer's life easy.
-                        Detailed description about the service that how it makes the customer's life easy.
-                    </p>
-                    <p>
-                        Detailed description about the service that how it makes the customer's life easy.
-                        Detailed description about the service that how it makes the customer's life easy.
-                        Detailed description about the service that how it makes the customer's life easy.
-                        Detailed description about the service that how it makes the customer's life easy.
-                    </p>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="span10 offset1">
-                    <hr>
-                    <div class="span3">
-                        <p>
-                            Ready to start the innovation?
-                        </p>
-                        <a class="btn btn-large btn-block" href="#">Try now</a>
-                    </div>
-                    <div class="span3">
-                        <p>
-                            Got confused?
-                        </p>
-                        <a class="btn btn-large btn-block" href="contact_us.html">Contact us</a>
-                    </div>
-                    <div class="span3">
-                        <p>
-                            Check frequently asked question
-                        </p>
-                        <a class="btn btn-large btn-block" href="../Additional%20Pages/faq.html">FAQ</a>
-                    </div>
-                </div>
-            </div>
-        </article> -->
 
         <div id="maindiv">
 
@@ -167,68 +114,6 @@
 <!-- End: Main content -->
 <!-- Start: FOOTER -->
 <footer>
-    <div class="container">
-        <div class="row">
-            <div class="span2">
-                <h4><i class="icon-star icon-white"></i> Home</h4>
-                <nav>
-                    <ul class="quick-links">
-                        <li><a href="about_us.php">About us</a></li>
-                        <li><a href="institutes.php">Institutes</a></li>
-                    </ul>
-                </nav>
-                <h4><i class="icon-cogs icon-white"></i> Members</h4>
-                <nav>
-                    <ul class="quick-links">
-                        <li><a href="education.php">Education</a></li>
-                        <!--<li><a href="resources.php">Resources</a></li>-->
-                        <li><a href="news.php">News</a></li>
-                        <li><a href="events.php">Events</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="span2">
-                <h4><i class="icon-beaker icon-white"></i> About</h4>
-                <nav>
-                    <ul class="quick-links">
-                        <li><a href="about_us.php">About us</a></li>
-                    </ul>
-                </nav>
-                <h4><i class="icon-thumbs-up icon-white"></i> Includes</h4>
-                <nav>
-                    <ul class="quick-links">
-                        <li><a href="photo_gallery.php">Photo Gallery</a></li>
-                        <li><a href="contact_us.php">Contact us</a></li>
-                        <li><a href="signin.php">Sign in</a></li>
-                        <li><a href="signup.php">Join now</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="span3">
-                <h4>Get in touch</h4>
-                <div class="social-icons-row">
-                    <a href="#"><i class="icon-twitter"></i></a>
-                    <a href="#"><i class="icon-facebook"></i></a>
-                    <a href="#"><i class="icon-linkedin"></i></a>
-                </div>
-                <div class="social-icons-row">
-                    <a href="#"><i class="icon-google-plus"></i></a>
-                    <a href="#"><i class="icon-github"></i></a>
-                    <a href="mailto:nts@gmail.com"><i class="icon-envelope"></i></a>
-                </div>
-                <div class="social-icons-row">
-                    <i class="icon-phone icon-large phone-number"></i> +94 11 2 825409
-                </div>
-            </div>
-            <div class="span3">
-                <h4>Get updated by email</h4>
-                <form>
-                    <input type="text" name="email" placeholder="Email address">
-                    <input type="submit" class="btn btn-primary" value="Subscribe">
-                </form>
-            </div>
-        </div>
-    </div>
     <hr class="footer-divider">
     <div class="container">
         <p>
---
+++ pages/resources/templates/colorboxScripts.php
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="<?php echo $path; ?>"></script>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("a[rel='colorbox']").colorbox({maxWidth: "100%", maxHeight: "100%", opacity: ".5"});
+    });
+</script>
